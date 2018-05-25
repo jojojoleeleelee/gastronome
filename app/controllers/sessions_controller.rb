15d24f5f54717290bd12d9c50e6cc3b8eb9d9cda
Joanne Lee <vernapii@gmail.com>
2018-05-25 01:08:41 -0700

    oath initial setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,34 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:user][:email])
-    if @user && @user.authenticate(params[:user][:password])
+    if request.env['omniauth.auth']
+      user = User.find_by(email: request.env['omniauth.auth']['info']['email'])
+      if !user
+        flash[:notice] = "Email does not match."
+        redirect_to sessions_create_path
+      else
       session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Welcome back to Gastronome!"
+      end
     else
-      redirect_to new_user_path
+        @user = User.find_by(email: params[:user][:email])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Welcome back to Gastronome!"
+      else
+        redirect_to new_user_path
+      end
     end
   end
 
   def destroy
     session.delete(:user_id)
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
 
+  get '/auth/google/callback' => 'sessions#create'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   post 'logout', to: 'sessions#destroy'
