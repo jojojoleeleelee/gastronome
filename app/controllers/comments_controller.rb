241f797ebfab80b950bfe26e36c5c0c9866cd47b
Joanne Lee <vernapii@gmail.com>
2018-08-09 12:13:20 -0700

    more specifications for comment table

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,66 @@
+class CommentsController < ApplicationController
+
+  def index
+    set_recipe
+    authorize @recipe, :show_comments?
+    @comments = @recipe.all_comments
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @comments }
+    end
+
+  end
+
+  def create
+    set_recipe
+    @comment = Comment.new(comment_params)
+    @comment.commenter = current_user
+    if @comment.save
+      flash[:notice] = "Successfully posted a comment"
+    else
+      flash[:notice] = @comment.errors.full_messages.to_sentence
+    end
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+    set_recipe
+    set_comment
+  end
+
+  def update
+    set_recipe
+    set_comment
+
+    if @comment.update(comment_params)
+      flash[:notice] = "Successfully posted a recipe"
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:alert] = @comment.errors.full_messages.to_sentence
+      redirect_to recipe_comment_path(@recipe,@comment)
+    end
+  end
+
+  def destroy
+    set_recipe
+    set_comment
+    @comment.destroy
+    flash[:notice] = "Comment was deleted"
+		redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def comment_params
+    params.require("comment").permit(:recipe_id, :content)
+  end
+
+  def set_recipe
+    @recipe = Prayer.find(params[:recipe_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :recipe
+  validates :content, presence: true
+  
+  belongs_to :recipe, class_name: "Recipe", foreign_key: "recipe_id"
+
+  def time
+    self.created_at.strftime("%m/%d/%y at %I:%M%p %Z")
+  end
 end
