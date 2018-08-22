9889894ac5a533f92adc1829e4e2cf0ad7684db1
Joanne Lee <vernapii@gmail.com>
2018-08-22 13:59:27 -0700

    routes to new signup and signin functionality using jQuery

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,8 +1,8 @@
-
 $(function(){
   $("a#signup").on("click", function(e){
     $.get(this.href).success(function(response){
       $("div#access").html(response)
+      console.log(response)
     })
     e.preventDefault();
   })
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,7 @@ $(function(){
   $("a#signin").on("click", function(e){
     $.get(this.href).success(function(response){
       $(document).html(response)
+      console.log(response)
     })
     e.preventDefault();
   })
