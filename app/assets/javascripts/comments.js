5e7d24d0bd52ebd5f13f524d9a75fd6662f7e8ce
Joanne Lee <vernapii@gmail.com>
2018-08-23 10:06:42 -0700

    recipe comments finally working

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -5,6 +5,31 @@ $(function() {
     var url = this.action;
     console.log(this)
     const data = $(this).serialize();
+    //debugger
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: data,
+      dataType: "json",
+      success: function(res) {
+        $("#comment-text").val("");
+        let newComment = new Comment(res.id, res.recipe_id, res.text);
+        let formatComment = newComment.formatText();
+        $(".commentsList").append(formatComment)
+        $("#comment-submit").attr("disabled", false)
+      }
+    });
+  });
 
-  })
-})
+  function Comment(id, recipeId, text) {
+    this.id = id;
+    this.recipeId = recipeId;
+    this.text = text;
+  }
+
+  Comment.prototype.formatText = function() {
+    let commentHtml = '';
+    commentHtml += `<li>${this.text}</li>`
+    return commentHtml;
+  }
+});
